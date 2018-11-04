8942f5889e1fa0888f362e5c64eba0603e95417f
Mustafa Qamar-ud-Din <mustafa.mahrous.89@gmail.com>
2018-11-04 08:00:12 +0200

    init

---
+++ main.py
@@ -0,0 +1,14 @@
+import pandas as pd
+from matplotlib import pyplot as plt
+import numpy as np
+from strategies.run_zipline import run_strategy
+
+
+def main():
+    print("*** PackPub - Hands-on Machine Learning for Algorithmic Trading Bots ***")
+    perf = run_strategy('buy_and_hold')
+    perf.to_csv("buy_and_hold.csv")
+
+
+if __name__ == '__main__':
+    main()
